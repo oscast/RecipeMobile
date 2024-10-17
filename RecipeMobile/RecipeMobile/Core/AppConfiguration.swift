672e5b577fefe9f7f65e920fd0f87fcb2ca0f780
Oscar Castillo <goldoac@live.com>
2024-10-17 14:21:19 -0600

    adds endpoint protocol
    adds the endpoint that is being injected for any request

---
+++ RecipeMobile/RecipeMobile/Core/AppConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  AppConfiguration.swift
+//  RecipeMobile
+//
+//  Created by Oscar Castillo on 17/10/24.
+//
+
+import Foundation
+
+extension URL {
+    static var cloudFront: URL {
+        URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/")!
+    }
+}
---
+++ RecipeMobile/RecipeMobile/Services/Requester/Endpoints/RecipeEndpoint.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeEndpoint.swift
+//  RecipeMobile
+//
+//  Created by Oscar Castillo on 17/10/24.
+//
+
+import Foundation
+
+struct RecipeEndpoint: Endpoint {
+    var baseURL: URL { .cloudFront }
+    var path: String { "recipes.json" }
+    var method: HTTPMethod { .post }
+    var headers: [String: String]?
+    var queryParams: [String: String]?
+    var body: Data?
+}
